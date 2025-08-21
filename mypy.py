1bf32a57a945c2449334219da6707f1b363b47ef
hanifYusoff <hanifUMK@gmail.com>
2025-08-21 11:54:58 +0800

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-    st.title("My First Streamlit App")
+    st.title("""My First Streamlit App""")
     x = st.slider("Select a value", 0, 100)
     st.write(f"You selected: {x}")
